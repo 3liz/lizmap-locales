954b5110560a675d353e601bf7ae27748935bd8c
rldhont <rldhont@gmail.com>
2018-10-17 12:28:54 +0200

    Add language mapping and Norwegian support

--- web-client/module_list.sh
+++ web-client/module_list.sh
@@ -3,10 +3,10 @@
 export MODULES="jelix master_admin jacl2 jacl2db jacl2db_admin jauthdb_admin jcommunity admin dataviz view"
 
 # all locales that are available into transifex
-export AVAILABLE_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SL"
+export AVAILABLE_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SL"
 
 # locales that are almost complete and so, that should be pushed to Lizmap
-export OFFICAL_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SL"
+export OFFICAL_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SL"
 
 export LOCALES_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 
